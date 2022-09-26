59a8351220c72ee507baa8e0e1c4c250445bf58e
ysnblt <106676214+ysnblt@users.noreply.github.com>
2022-09-26 21:18:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Parametre alan geriye değer döndüren metod
+            // return anahtar kelimesi
+            // metod sonu
+
+            int donenDeger = toplamaYap(6, 7);
+            Console.WriteLine("Sonuç = " + donenDeger);
+            Console.ReadLine();
+        }
+
+        static int toplamaYap(int sayi1 , int sayi2)
+        {
+
+            int sonuc = sayi1 + sayi2;
+            return sonuc;
+
+        }
+    }
+}
